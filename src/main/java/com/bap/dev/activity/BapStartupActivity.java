2fd8f9cdf0f539939bb0c5221343387c532a93f4
LHR-1112 <2991747768@qq.com>
2025-12-01 10:26:43 +0800

    修改：支援至2022.3.2版本

--- src/main/java/com/bap/dev/activity/BapStartupActivity.java
+++ src/main/java/com/bap/dev/activity/BapStartupActivity.java
@@ -7,39 +7,22 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.ProjectActivity;
 import com.intellij.openapi.startup.StartupActivity;
-import kotlin.Unit;
-import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-// 1. 实现 ProjectActivity 接口
-public class BapStartupActivity implements ProjectActivity, StartupActivity {
-
-    // 2. 实现 execute 方法。
-    // 注意：因为这是 Kotlin 的 suspend 函数，Java 中需要多接收一个 Continuation 参数，并返回 Object
-    @Nullable
-    @Override
-    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
-        // --- 你的原有逻辑开始 ---
-        BapDocumentListener listener = new BapDocumentListener(project);
-
-        // 注册监听器
-        EditorFactory.getInstance().getEventMulticaster().addDocumentListener(listener, project);
-        // --- 你的原有逻辑结束 ---
-
-        // 3. 必须返回 Kotlin 的 Unit.INSTANCE
-        return Unit.INSTANCE;
-    }
+// 使用旧版接口，兼容性好，代码简单
+public class BapStartupActivity implements StartupActivity {
 
     @Override
     public void runActivity(@NotNull Project project) {
-        // 启动后台任务刷新所有模块
+        // 1. 注册文档监听器
+        BapDocumentListener listener = new BapDocumentListener(project);
+        EditorFactory.getInstance().getEventMulticaster().addDocumentListener(listener, project);
+
+        // 2. 启动后台任务刷新所有模块
         ProgressManager.getInstance().run(new Task.Backgroundable(project, "Refreshing Bap Modules...", true) {
             @Override
             public void run(@NotNull ProgressIndicator indicator) {
-                // 调用 ProjectRefresher 的全量刷新方法
                 new ProjectRefresher(project).refreshAllModules();
             }
         });
