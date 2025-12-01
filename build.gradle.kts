02fa4008f7b6eac369f8598ae40011018466cb17
LHR-1112 <2991747768@qq.com>
2025-12-01 10:26:43 +0800

    修改：支援至2022.3.2版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.bap.dev"
-version = "1.1"
+version = "1.1.1"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,8 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
-        create("IC", "2025.1")
+//        create("IC", "2025.1")
+        create("IC", "2022.3.2") // 建议使用 2022.3 系列的一个具体版本，例如 2022.3.4
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
         // --- ✅ 关键修改：添加这一行 ---
@@ -34,7 +35,14 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "251"
+//            sinceBuild = "251"
+// 1. 兼容下限保持 223，确保 2022.3 版本能安装
+            sinceBuild = "223"
+
+            // 2. 关键修改：设置一个兼容上限，包含您的 251.x 版本
+            // '252.*' 表示兼容到 2025.2 版本，即包含 251.x
+            // 如果您想要更久远的兼容性，可以设置更高的版本号，如 '999.*'
+            untilBuild = "999.*"
         }
 
         changeNotes = """
@@ -46,10 +54,13 @@ intellijPlatform {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
+//        sourceCompatibility = "21"
+//        targetCompatibility = "21"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "21"
+//        kotlinOptions.jvmTarget = "21"
+        kotlinOptions.jvmTarget = "17"
     }
 }
